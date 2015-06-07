ecee7ee4b6fcd73383ad5cb17c2dfc61f3f5f3d8
Pyotr Ermishkin <quasiyoke@gmail.com>
2015-06-07 22:36:50 +0300

    Unicode hashtags support was added. PEP8 styling was partially applied.

--- InstaBot.py
+++ InstaBot.py
@@ -1,4 +1,4 @@
-import mechanize, yaml, re, time, sys, pycurl, hmac
+import mechanize, yaml, re, time, sys, pycurl, hmac, urllib
 from hashlib import sha256
 
 WEBSTA_URL = "http://websta.me/"
@@ -9,7 +9,6 @@ USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Ge
 
 # Function to encode the string with the IP and ID of the picture then like it
 def encodeAndRequest(id):
-
 	c = pycurl.Curl()
 	signature = hmac.new(str(profile['CREDENTIALS']['CLIENT_SECRET']), profile['IP'], sha256).hexdigest()
 	header = '|'.join([profile['IP'], signature])
@@ -28,10 +27,8 @@ def encodeAndRequest(id):
 
 # Function to parse the Top HashTag page and get the current top hashtags
 def getTopHashTags(br):
-
 	br.open(WEBSTA_HASHTAG)
 	topHashtags = re.findall('\"\>#(.*)\<\/a\>\<\/strong\>', br.response().read())
-
 	return topHashtags
 	
 # Function to read the hashtags from a users file if not wanting to parse the top 100
@@ -42,55 +39,54 @@ def getHashtagsFromFile():
     #Hashtag file input
     f = open(filename)
     #strips newline character
-    hashtags = [line.strip() for line in open(filename)]
+    hashtags = [unicode(line.strip(), 'utf-8') for line in open(filename)]
     f.close()
     return hashtags
 	
 # Function to like hashtages
 def like(br, hashtags):
+    likes = 0
 
-	likes = 0
+    for hashtag in hashtags:
+        hashtaglikes = 0
+        media_id = []
+        response = br.open(WEBSTA_URL +"tag/" + urllib.quote(hashtag.encode('utf-8')))
+        print u"Liking #%s" % hashtag
+        media_id = re.findall("span class=\"like_count_(.*)\"", response.read())
 
-	for hashtag in hashtags:
-		hashtaglikes = 0
-		media_id = []
-		response = br.open(WEBSTA_URL +"tag/" + hashtag)
-		print "Liking #" + str(hashtag)
-		media_id = re.findall("span class=\"like_count_(.*)\"", response.read())
+        for id in media_id:
 
-		for id in media_id:
+            if profile['MAXLIKES'] == "NO_MAX":
+                pass
+            elif likes >= int(profile['MAXLIKES']):
+                print "You have reached MAX_LIKES(" + str(profile['MAXLIKES']) + ")"
+                print u"This # is currently %s" % hashtag
+                sys.exit()
+                break
 
-			if profile['MAXLIKES'] == "NO_MAX":
-				pass
-			elif likes >= int(profile['MAXLIKES']):
-				print "You have reached MAX_LIKES(" + str(profile['MAXLIKES']) + ")"
-				print "This # is currently " + str(hashtag)
-				sys.exit()
-				break
+            if profile['PERHASHTAG'] == "NO_MAX":
+                pass
+            elif hashtaglikes >= int(profile['PERHASHTAG']):
+                print "REACHED MAX_LIKES PER HASHTAG"
+                print "MOVING ONTO NEXT HASHTAG"
+                hashtaglikes = 0
+                break
 
-			if profile['PERHASHTAG'] == "NO_MAX":
-				pass				
-			elif hashtaglikes >= int(profile['PERHASHTAG']):
-				print "REACHED MAX_LIKES PER HASHTAG"
-				print "MOVING ONTO NEXT HASHTAG"
-				hashtaglikes = 0
-				break
+            response = encodeAndRequest(id)
 
-			response = encodeAndRequest(id)
+            if bool(re.search("200", response)):
+                print " YOU LIKED " + str(id)
+                likes += 1
+                hashtaglikes += 1
+                time.sleep(profile['SLEEPTIME'])
+            else:
+                print "SOMETHING WENT WRONG"
+                print response
+                print "SLEEPING FOR 60 seconds"
+                print "CURRENTLY LIKED " + str(likes) + " photos"
+                time.sleep(60)
 
-			if bool(re.search("200", response)):
-				print " YOU LIKED " + str(id)
-				likes += 1
-				hashtaglikes += 1
-				time.sleep(profile['SLEEPTIME'])
-			else:
-				print "SOMETHING WENT WRONG"
-				print response
-				print "SLEEPING FOR 60 seconds"
-				print "CURRENTLY LIKED " + str(likes) + " photos"
-				time.sleep(60)
-		
-	print "YOU LIKED " + str(likes) + " photos"
+    print "YOU LIKED " + str(likes) + " photos"
 
 if __name__ == "__main__":
 
